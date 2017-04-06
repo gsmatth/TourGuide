fcad8e6fc6c31e2b051297bda5e06ce268cff6f9
Doug Popadince <gsmatth@icloud.com>
2017-04-06 09:49:56 -0700

    added basic pageadapter

---
+++ app/src/main/java/com/example/android/tourguide/TourFragmentPagerAdapter.java
@@ -0,0 +1,31 @@
+package com.example.android.tourguide;
+
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Created by djp on 4/6/17.
+ */
+
+public class TourFragmentPagerAdapter extends FragmentPagerAdapter {
+    public TourFragmentPagerAdapter(FragmentManager fm)(super(fm);)
+
+    @Override
+    public Fragment getItem(int position){
+        if(position == 0){
+            return new DiningFragment();
+        }
+    }
+
+    @Override
+    public int getCount(){return 1;}
+    
+    @Override
+    public CharSequence getPageTitle(int position){
+        if(position == 0){
+            return "dining";
+        } else {
+            return "dining";
+        }
+    }
+}
